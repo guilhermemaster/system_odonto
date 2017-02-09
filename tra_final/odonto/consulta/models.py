bd0541e351e40bbb03276b07b65ff09ee6565669
Mauricio Eduardo <olive.y.ra@hotmail.com>
2017-02-09 18:03:45 -0300

    add consulta e historico da consulta

--- tra_final/odonto/consulta/models.py
+++ tra_final/odonto/consulta/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import datetime
+
 class Dentist(models.Model):
 
     name = models.CharField('Nome', max_length=100)
@@ -20,7 +22,6 @@ class Patient(models.Model):
 
     name = models.CharField('Nome', max_length=100)
     cpf = models.IntegerField('CPF')
-    dentist = models.ForeignKey('consulta.Dentist', verbose_name='Dentista')
     rg = models.IntegerField('RG')
     phone = models.IntegerField('Telefone')
 
@@ -33,4 +34,21 @@ class Patient(models.Model):
         ordering = ['name']
 
     def __str__(self):
-        return self.name        
+        return self.name
+
+
+class Consultation(models.Model):
+
+    data = models.DateField(blank=True, null=True)
+    name = models.ForeignKey('consulta.Patient', verbose_name='Nome do Paciente')
+    dentist = models.ForeignKey('consulta.Dentist', verbose_name='Dentista')
+    description = models.TextField('Historico', blank=True)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+
+    class Meta:
+        verbose_name = 'Consulta'
+        verbose_name_plural = 'Consulta'
+        ordering = ['dentist']
